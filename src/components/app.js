9368633c759f7c9b34f4d46a38dd6c4af6abdf38
c-soriano <celine.soriano.1@gmail.com>
2017-09-29 00:57:49 -0500

    Calculator setup

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CalculatorContainer from './container-calculator';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <CalculatorContainer />
+            </div>
+        );
+  }
+}
---
+++ src/components/container-calculator.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CalculatorContainer extends Component {
+    render() {
+        return (
+            <div id="calculatorContainer">
+                Calculator Container
+            </div>
+        );
+    }
+}
+
+export default CalculatorContainer;
